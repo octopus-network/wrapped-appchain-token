ab498861f39bccdf59c7a877b8cba94c729935c0
Rivers Yang <rivers.yang@icloud.com>
2022-06-09 21:06:02 +0800

    Update cargo dependencies.

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 if [ ! -d "out" ]; then
     mkdir -p "out"
 fi
-cp target/wasm32-unknown-unknown/release/wrapped_appchain_token.wasm ./out/main.wasm
+cp target/wasm32-unknown-unknown/release/wrapped_appchain_token.wasm ./out/
